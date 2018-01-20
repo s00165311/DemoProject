d6c0a9053898176a6121952c00f8582b5cb47da2
s00165311 <s00165311@mail.itsligo.ie>
2018-01-20 01:38:53 +0000

    Add project files.

---
+++ ProjectDemo/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ProjectDemo
+{
+    [Activity(Label = "ProjectDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
