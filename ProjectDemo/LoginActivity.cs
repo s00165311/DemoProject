7f60c13bb855a48c234ac9228a0c4e8807795f6f
s00165311 <s00165311@mail.itsligo.ie>
2018-01-20 06:06:59 +0000

    Added Second Activity

--- ProjectDemo/LoginActivity.cs
+++ ProjectDemo/LoginActivity.cs
@@ -15,12 +15,27 @@ namespace ProjectDemo
     [Activity(Label = "LoginActivity", MainLauncher = true)]
     public class LoginActivity : Activity
     {
+        Button gButton;
+        LinearLayout gLinearLayout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Login);
+
+            gLinearLayout = FindViewById<LinearLayout>(Resource.Id.loginView);
+            gButton = FindViewById<Button>(Resource.Id.btnLogin);
+
+            gButton.Click += GButton_Click;
+        }
+
+        private void GButton_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            this.StartActivity(intent);
+
         }
     }
 }
--- ProjectDemo/Resources/Resource.Designer.cs
+++ ProjectDemo/Resources/Resource.Designer.cs
@@ -44,26 +44,32 @@ namespace ProjectDemo
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040005
-			public const int btnLogin = 2130968581;
-			
 			// aapt resource value: 0x7f040006
-			public const int btnSignUp = 2130968582;
+			public const int btnLogin = 2130968582;
 			
-			// aapt resource value: 0x7f040001
-			public const int lblEmail = 2130968577;
-			
-			// aapt resource value: 0x7f040003
-			public const int lblPassword = 2130968579;
+			// aapt resource value: 0x7f040007
+			public const int btnSignUp = 2130968583;
 			
 			// aapt resource value: 0x7f040002
-			public const int txtEmail = 2130968578;
-			
-			// aapt resource value: 0x7f040000
-			public const int txtHeader = 2130968576;
+			public const int lblEmail = 2130968578;
 			
 			// aapt resource value: 0x7f040004
-			public const int txtPassword = 2130968580;
+			public const int lblPassword = 2130968580;
+			
+			// aapt resource value: 0x7f040000
+			public const int loginView = 2130968576;
+			
+			// aapt resource value: 0x7f040008
+			public const int textView1 = 2130968584;
+			
+			// aapt resource value: 0x7f040003
+			public const int txtEmail = 2130968579;
+			
+			// aapt resource value: 0x7f040001
+			public const int txtHeader = 2130968577;
+			
+			// aapt resource value: 0x7f040005
+			public const int txtPassword = 2130968581;
 			
 			static Id()
 			{
